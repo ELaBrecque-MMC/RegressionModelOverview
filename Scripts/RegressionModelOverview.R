d7077ebba915fd61bb32a3e5d32d4be3759cee7a
meganferg <meganferg@gmail.com>
2021-12-07 15:23:24 -0800

    Finally figured out how to change legend labels.

--- Scripts/RegressionModelOverview.R
+++ Scripts/RegressionModelOverview.R
@@ -1,4 +1,4 @@
-#Script RegressionModelOverview.r...Megan C. Ferguson...6 December 2021
+#Script RegressionModelOverview.r...Megan C. Ferguson...7 December 2021
 
   library(ggplot2)
 
@@ -20,6 +20,63 @@
         geom_line(aes(y = dens2, colour = "dens2")) +
         xlab("Y") +
         ylab("PDF") + 
-        theme(legend.position = "none")
+        scale_color_hue(labels=c("mean=0, sd=1", 
+                                 "mean=3, sd=0.6")) +
+        theme(legend.title = element_blank(),
+              legend.text = element_text(size=14))        
       ggsave(filename="Figures\\2_Normal_Distributions.png",
              device="png", width=7.5, height=6.5, units="in")
+      
+  #What is the binomial distribution?
+
+    #Plot the probability density functions for three Bin distributions:
+    #dens1: p=0.5, n=20
+    #dens2: p=0.7, n=20
+    #dens3: p=0.5, n=40
+      
+      x <- seq(from=0, to=40, by=1)
+      
+      dens1 <- dbinom(x,
+                     size=20,
+                     prob = 0.5)
+      
+      dens2 <- dbinom(x,
+                     size=20,
+                     prob = 0.7)
+      
+      dens3 <- dbinom(x,
+                     size=40,
+                     prob = 0.5)
+      
+      df.dens <- cbind.data.frame(x, dens1, dens2, dens3)
+      
+      ggplot(df.dens, aes(x)) +
+        geom_point(aes(y = dens1, colour = "dens1")) + 
+        geom_point(aes(y = dens2, colour = "dens2")) +
+        geom_point(aes(y = dens3, colour = "dens3")) +
+        xlab("Y") +
+        ylab("p(Y = y)") + 
+        scale_color_hue(labels=c("p=0.5, n=20", 
+                                 "p=0.7, n=20",
+                                 "p=0.5, n=40")) +
+        theme(legend.title = element_blank(),
+              legend.text = element_text(size=14))
+      ggsave(filename="Figures\\3_Binomial_Distributions.png",
+             device="png", width=7.5, height=6.5, units="in")
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
